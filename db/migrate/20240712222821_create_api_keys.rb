c346d70996d6162fd267b81c3398544ff3709358
Cloud <anclaudysdata@gmail.com>
2024-07-25 20:26:11 +0000

    add verified_idenity model, rearrange api_keys migrations to remove error, change back to development

--- db/migrate/20240712222821_create_api_keys.rb
+++ db/migrate/20240712222821_create_api_keys.rb
@@ -1,9 +1,9 @@
-class CreateApiKeys < ActiveRecord::Migration[7.1]
+class CreateApiKeys < ActiveRecord::Migration[6.1]  # version might be different
   def change
     create_table :api_keys do |t|
       t.string :service_name
       t.string :private_key
-
+      
       t.timestamps
     end
   end
